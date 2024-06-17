be6ac15f0d1344a3ac03eafb917282a8b9c7f86f
aryanchy449 <108731844+aryanchy449@users.noreply.github.com>
2024-06-17 12:07:33 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -7,25 +7,25 @@ load_dotenv()
 
 
 class Var(object):
-    API_ID = int(getenv('API_ID'))
-    API_HASH = str(getenv('API_HASH'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN'))
+    API_ID = int(getenv('API_ID',"23080322"))
+    API_HASH = str(getenv('API_HASH',"b3611c291bf82d917637d61e4a136535"))
+    BOT_TOKEN = str(getenv('BOT_TOKEN',"6931048916:AAEiwn5cej-PVBw95cqsGskW-BEGx95o4jY"))
     SESSION_NAME = str(getenv('SESSION_NAME', 'F2LxBot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL',"gfyfuvicuchc"))
     PORT = int(getenv('PORT', 8080))
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
-    OWNER_ID = int(getenv('OWNER_ID', '2068472689'))
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', "https://miniature-gae-telegram-bots-7a6a60ef.koyeb.app/"))
+    OWNER_ID = int(getenv('OWNER_ID', 6214889840))
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME',"aryanchy449"))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
     else:
         ON_HEROKU = False
-    FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
+    FQDN = str(getenv('FQDN',"https://miniature-gae-telegram-bots-7a6a60ef.koyeb.app/")) if not ON_HEROKU or getenv('FQDN',""https://miniature-gae-telegram-bots-7a6a60ef.koyeb.app/) else APP_NAME+'.herokuapp.com'
     URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
         "http://{}:{}/".format(FQDN, PORT)
     DATABASE_URL = str(getenv('DATABASE_URL'))
