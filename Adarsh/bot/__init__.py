50a56ee339b4eae55a56fa70e8accfae6b083c61
aryanchy449 <108731844+aryanchy449@users.noreply.github.com>
2024-06-17 12:27:09 +0530

    Update __init__.py

--- Adarsh/bot/__init__.py
+++ Adarsh/bot/__init__.py
@@ -3,7 +3,6 @@ from pyrogram import Client
 from ..vars import Var
 
 StreamBot = Client(
-    session_name="hello",
     api_id=Var.API_ID,
     api_hash=Var.API_HASH,
     bot_token=Var.BOT_TOKEN,
